7b00df70d052215dd186486fb618a2bf49a40101
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-09 14:53:45 +0100

    [FIX] fix missing dependency

--- pos_sale_order/__manifest__.py
+++ pos_sale_order/__manifest__.py
@@ -16,6 +16,7 @@
         "account_reconcile_restrict_partner_mismatch",
         "web_widget_numeric_step",
         "sale_delivery_state",
+        "pos_backend_partner",
     ],
     "data": [
         "security/res_groups.xml",
