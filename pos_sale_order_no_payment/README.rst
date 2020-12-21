e3958fb8983a19dbd5ae6a159c396336265a6a37
hparfr <raph@hpar.fr>
2020-12-21 19:11:26 +0100

    Add pos_sale_order_no_payment

---
+++ pos_sale_order_no_payment/README.rst
@@ -0,0 +1,13 @@
+POS Sale Order No Payment
+=========================
+
+This module created a sale order in POS.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Raphaël Reverdy <raphael.reverdy@akretion.com>
---
+++ pos_sale_order_no_payment/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Akretion (https://www.akretion.com).
+# @author Raphaël Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS Sale Order No Payment",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/pos-sale-order",
+    "license": "AGPL-3",
+    "depends": [
+        "pos_sale_order",
+    ],
+    "data": [
+        "templates/assets.xml",
+        "views/product_pricelist.xml",
+    ],
+    "qweb": [
+        "static/src/xml/QuotationButton.xml",
+        "static/src/xml/PaymentScreen.xml",
+        "static/src/xml/PaymentScreenStatus.xml",
+        "static/src/xml/PaymentScreenNumpad.xml",
+    ],
+    "installable": True,
+}
