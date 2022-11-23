d9769546476b5453495fa9ac212a2d8301d2176c
Pierrick Brun <pierrick.brun@akretion.com>
2022-11-23 10:43:06 +0100

    [ADD] pos_sale_order_hr

---
+++ pos_sale_order_hr/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "PoS Sale Order HR",
+    "summary": "Glue module for HR and pos_sale_order",
+    "version": "14.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://github.com/akretion/pos-sale-order",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "pos_sale_order",
+        "pos_hr",
+    ],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
