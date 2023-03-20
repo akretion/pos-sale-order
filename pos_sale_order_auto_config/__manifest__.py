edbcc4fd8166f4375495e4eb46474fee4ef96565
chafique.delli <chafique.delli@akretion.com>
2023-03-20 23:45:41 +0100

    [14.0][ADD] pos_sale_order_auto_config

---
+++ pos_sale_order_auto_config/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Chafique Delli <chafique.delli@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS Sale Order Auto Config",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "Akretion",
+    "website": "https://github.com/akretion/pos-sale-order",
+    "license": "AGPL-3",
+    "depends": [
+        "pos_sale_order",
+    ],
+    "data": [
+        "views/pos_config.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+}
