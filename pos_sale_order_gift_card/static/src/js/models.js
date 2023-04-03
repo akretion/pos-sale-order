765ae84de511844c3b70fa0b2ab61c2038da3816
Pierrick Brun <pierrick.brun@akretion.com>
2023-04-03 10:42:00 +0200

    [FIX] show gift card for every language

--- pos_sale_order_gift_card/static/src/js/models.js
+++ pos_sale_order_gift_card/static/src/js/models.js
@@ -25,7 +25,7 @@ odoo.define("pos_sale_order_gift_card.models", function (require) {
             posmodel_super._after_orders_received.apply(this, arguments);
             var self = this;
             res.orders.forEach(function (order) {
-                if ("Order " + self.get_order().uid === order.pos_reference) {
+                if (order.pos_reference.endsWith(" " + self.get_order().uid)) {
                     self.get_order().bought_gift_cards = order.bought_gift_cards;
                 }
             });
